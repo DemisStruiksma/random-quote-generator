fb6554e57c5a4112f5e273cbf8113ec9f0fa42e1
Demis Struiksma <contact@demis.io>
2019-06-10 19:57:14 +0200

    Added quotes to quotes array and added "getRandomQuote" function

--- js/script.js
+++ js/script.js
@@ -15,13 +15,34 @@ project 1 - A Random Quote Generator
   Use console.log() to log your array of quotes to the console.
 ***/
 
-var quote = [
-    {}
-    {}
-    {}
-    {}
-    {}
+var quotes = [
+    {
+      quote: "Music is the greatest communication in the world. Even if people don't understand the language that you're singing in, they still know good music when they hear it.",
+      source: "-Lou Rawis"
+    },
+    {
+      quote:"You know you’re in love when you can’t fall asleep because reality is finally better than your dreams.",
+      source: "-Dr. Suess"
+    },
+    {
+      quote: "I’m selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can’t handle me at my worst, then you sure as hell don’t deserve me at my best.",
+      source: "-Marilyn Monroe"
+    },
+    {
+      quote: "If you concentrate on what you have, you will always end up having more. If you focus on what you don't have, you will never, ever have enough.",
+      source: "-Oprah",
+      citation: "Lifeclass",
+      year: "2012"
+    },
+    {
+      quote: "Stay Hungry. Stay Foolish",
+      source: "-Steve Jobs",
+      citation: "Stanford University",
+      year: "2005"
+    }
 ];
+
+console.log(quotes);
 
 
 
@@ -33,7 +54,9 @@ var quote = [
    - Cse the random number to `return` a random quote object from the `quotes` array.
 ***/
 
-
+function getRandomQuote() {
+  var randomNumber;
+}
 
 
 /***
@@ -59,7 +82,7 @@ var quote = [
   comment.
 ***/
 
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+// document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
